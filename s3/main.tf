46c0259ceacd7d1c1b1bb2e7fa3adbcac0cdf0e2
rageshganeshkumar <ragesh.ganeshkumar@hashicorp.com>
2025-04-11 13:44:02 +0530

    init

--- s3/main.tf
+++ s3/main.tf
@@ -1,5 +1,5 @@
 resource "random_pet" "increase_resource_count" {
-  for_each = { for idx in range(1, 301) : idx => idx }
+  for_each = { for idx in range(1, 10) : idx => idx }
 
   prefix = "hello-world-lambda-${each.key}"
   length = 2
