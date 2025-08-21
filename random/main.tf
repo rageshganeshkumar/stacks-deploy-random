905381a23601716df9bc0b4e28b9a4f4f508022e
rageshganeshkumar <ragesh.ganeshkumar@hashicorp.com>
2025-08-21 13:12:29 +0530

    push

--- random/main.tf
+++ random/main.tf
@@ -1,6 +1,27 @@
-resource "random_pet" "increase_resource_count" {
-  for_each = { for idx in range(1, 61) : idx => idx }
+variable "prefix_override" {
+  description = "Override prefix for random pets from varset"
+  type        = string
+  default     = "hello-world"
+}
 
-  prefix = "hello-world-${each.key}"
+variable "test_message" {
+  description = "Test message from varset"
+  type        = string
+  default     = "default-message"
+}
+
+resource "random_pet" "increase_resource_count" {
+  for_each = { for idx in range(1, 11) : idx => idx }
+
+  prefix = "${var.prefix_override}-${each.key}"
   length = 4
+}
+
+# Output to show the varset values are being used
+output "test_message" {
+  value = var.test_message
+}
+
+output "prefix_used" {
+  value = var.prefix_override
 }
