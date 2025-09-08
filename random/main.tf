20957f645b0fa4fb14a45f524141249ede9767d8
rageshganeshkumar <ragesh.ganeshkumar@hashicorp.com>
2025-09-08 14:40:40 +0530

    Update main.tf

--- random/main.tf
+++ random/main.tf
@@ -1,5 +1,5 @@
 resource "random_pet" "increase_resource_count" {
-  for_each = { for idx in range(1, 9) : idx => idx }
+  for_each = { for idx in range(1, 10) : idx => idx }
 
   prefix = "hello-world-${each.key}"
   length = 4
